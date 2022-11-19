aee4389fb7cd3b05ffbd641bacb6b93e15f29b68
okooijman <okooijman@neusta.de>
2022-11-19 12:29:18 +0100

    refactor(ldap) prepared ldapservice for DI

---
+++ Schuelerausweis/Schuelerausweis/Models/LdapConfig.cs
@@ -0,0 +1,20 @@
+﻿namespace Schuelerausweis.Models;
+
+public class LdapConfig
+{
+    public LdapCredetialsConfig Credentials { get; set; }
+    public LdapServerConfig Server { get; set; }
+}
+
+public class LdapCredetialsConfig
+{
+    public string Domain { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
+
+public class LdapServerConfig
+{
+    public string HostName { get; set; }
+    public int  Port { get; set; }
+}
---
+++ Schuelerausweis/Schuelerausweis/Validator/TokenDataValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Schuelerausweis.Models;
+
+namespace Schuelerausweis.Validator;
+
+public class TokenDataValidator : AbstractValidator<TokenData>
+{
+    public TokenDataValidator()
+    {
+        RuleFor(x => x.Id);
+        RuleFor(x => x.Token).
+    }
+}
